6e70eba1690bd02b99cca3b0dc4f8620c6335820
Michal <1michalbaran@gmail.com>
2020-01-16 19:39:54 +0100

    Dodanie MainActivity

--- app/src/main/java/com/michalbaran/incomeapp/LoginActivity.java
+++ app/src/main/java/com/michalbaran/incomeapp/LoginActivity.java
@@ -3,12 +3,20 @@ package com.michalbaran.incomeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editLogin;
+    EditText editPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        editLogin.findViewById(R.id.editLogin);
+        editPassword.findViewById(R.id.editPassword);
     }
 }
