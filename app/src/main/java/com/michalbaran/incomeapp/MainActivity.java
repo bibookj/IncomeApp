dacfb7a878f768a78efcfba9d34007c4b4b9acbf
Michal <1michalbaran@gmail.com>
2020-01-30 21:23:55 +0100

    Added new features to main activity, added icons and stuff

--- app/src/main/java/com/michalbaran/incomeapp/MainActivity.java
+++ app/src/main/java/com/michalbaran/incomeapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.michalbaran.incomeapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         addTransactiontBtn=findViewById(R.id.btnAddTransaction);
         addTransactiontBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
+
 }
