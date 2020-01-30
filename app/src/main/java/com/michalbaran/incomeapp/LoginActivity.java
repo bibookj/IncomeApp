fe0ef01f8067edfde3b665feb26b1a9a8f6a9929
Michal <1michalbaran@gmail.com>
2020-01-30 18:48:04 +0100

    Added links between activities on btn click

--- app/src/main/java/com/michalbaran/incomeapp/LoginActivity.java
+++ app/src/main/java/com/michalbaran/incomeapp/LoginActivity.java
@@ -2,7 +2,9 @@ package com.michalbaran.incomeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,5 +20,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_activity);
         editLogin=findViewById(R.id.editLogin);
         editPassword=findViewById(R.id.editPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
