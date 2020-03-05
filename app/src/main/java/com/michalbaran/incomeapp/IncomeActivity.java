4eefda42ef1754ed3751e6eb67d0a74cbd9554e6
Michal <1michalbaran@gmail.com>
2020-03-05 19:04:45 +0100

    Formating date in IncomeViewAdapter

--- app/src/main/java/com/michalbaran/incomeapp/IncomeActivity.java
+++ app/src/main/java/com/michalbaran/incomeapp/IncomeActivity.java
@@ -7,8 +7,16 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class IncomeActivity extends AppCompatActivity {
 
@@ -19,6 +27,7 @@ public class IncomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +36,17 @@ public class IncomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        List<Income> incomeList;
+        incomeList = new ArrayList<>();
+        incomeList.add(new Income("Food", Calendar.getInstance().getTime(),21.5));
+        incomeList.add(new Income("Travel", Calendar.getInstance().getTime(),22.1));
+        incomeList.add(new Income("Ship", Calendar.getInstance().getTime(),2000000.0));
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(new IncomeViewAdapter(this, incomeList));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
 }
