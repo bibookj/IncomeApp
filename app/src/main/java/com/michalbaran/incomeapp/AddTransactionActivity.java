d8239cf44d3a798be9462cb473245833ce3fb349
Michal <1michalbaran@gmail.com>
2020-05-09 10:32:07 +0200

    add: basic spinner functionality to AddTransactionActivity

--- app/src/main/java/com/michalbaran/incomeapp/AddTransactionActivity.java
+++ app/src/main/java/com/michalbaran/incomeapp/AddTransactionActivity.java
@@ -11,16 +11,19 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class AddTransactionActivity extends AppCompatActivity {
+public class AddTransactionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TextView txtDate;
     DatePickerDialog.OnDateSetListener dateSetListener;
@@ -29,6 +32,8 @@ public class AddTransactionActivity extends AppCompatActivity {
     Context context;
     EditText amountEdit, categoryEdit;
     char activityMode;
+    Spinner categorySpinner;
+    String categoryTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,8 +90,12 @@ public class AddTransactionActivity extends AppCompatActivity {
         //Toast.makeText(this, activityMode, Toast.LENGTH_SHORT).show();
 
         amountEdit = findViewById(R.id.editAmount);
-        categoryEdit = findViewById(R.id.editCategory);
-
+        //categoryEdit = findViewById(R.id.editCategory);
+        categorySpinner = findViewById(R.id.SpinnerCategory);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.SpinnerArray, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(adapter);
+        categorySpinner.setOnItemSelectedListener(this);
 
         txtDate = findViewById(R.id.txtDate);
         txtDate.setOnClickListener(new View.OnClickListener() {
@@ -120,7 +129,8 @@ public class AddTransactionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Double amount = Double.valueOf(amountEdit.getText().toString());
-                String category = categoryEdit.getText().toString();
+                //String category = categoryEdit.getText().toString();
+                String category = categoryTxt;
                 String date = txtDate.getText().toString();
 
                 Intent intent = new Intent();
@@ -132,6 +142,17 @@ public class AddTransactionActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        categoryTxt = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),categoryTxt,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
